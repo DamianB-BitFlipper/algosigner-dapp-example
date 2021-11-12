d28a79c2f5791cc08d7680a09c728d4df5c480f5
Damian Barabonkov <damianb@mit.edu>
2021-11-12 21:24:44 +0100

    WalletConnect example fully functional

--- tx-test/common/helper.js
+++ tx-test/common/helper.js
@@ -23,3 +23,24 @@ function toJsonReplace(key, value) {
     // Return without modification
     return value;
 }
+
+// https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
+function arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
+
+function base64ToArrayBuffer(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
